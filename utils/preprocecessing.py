a7d4d13b484eb8203d08f8e6a92ceaccf57e7e6d
Anthony <ngangaanthony31@gmail.com>
2026-07-17 17:22:21 +0300

    Preprocessing pipeline

--- utils/preprocecessing.py
+++ utils/preprocecessing.py
@@ -1,4 +1,30 @@
 # This utility will perform the following
 #1. Delete columns that are not needed
 #2. Map the device column to binary(smartphone: 0, feature: 1)
+#3. calls the encoder from ../models/encoder.pkl and applies it to preprocessed df
+
+def preprocessing(df):
+    def columns_dropper():
+        columns_to_drop = ['transaction_id', 'hour', 'day_of_week', 'month_2026', 'day',
+                            'sender_balance_after', 'receiver_balance_before',
+                            'receiver_balance_after']
+        df.drop(columns=columns_to_drop, inplace=True)
+        return df
+    
+    def device_mapper():
+        df['device_type'] = df['device_type'].map({'smartphone': 0, 'feature': 1})
+        return df
+
+    df = columns_dropper()
+    df = device_mapper()
+
+    return df
+
+def encode(df):
+    import joblib
+    encoder = joblib.load('../models/encoder.pkl')   #Load encoder
+    encoded_data = encoder.transform(df)   #Apply onehot encoder
+    
+    return encoded_data
+
 
