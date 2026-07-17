4ed99e92f42ec023ae010f6c283717a816bba3ec
Anthony <ngangaanthony31@gmail.com>
2026-07-17 16:59:29 +0300

    feature_engineering.py utility

--- utils/feature_engineering.py
+++ utils/feature_engineering.py
@@ -7,19 +7,32 @@ def feature_engineering(df):
 
     def day_mapper():
         mapper = {'Monday': 1, 'Tuesday': 2, 'Wednesday': 3, 'Thursday': 4, 'Friday': 5, 'Saturday': 6, 'Sunday': 7}
-        df['day_of_week' ] =  df['day'].map(mapper)
+        df['day'] =  df['day_of_week'].map(mapper)
         return df
 
-    def cyclic_encoder(directory):
+    def cyclic_encoder():
         directory = {
             'hour': 24,
-            'day_of_week': 7,
-            'month': 12
+            'day': 7,
+            'month_2026': 12
         }
         for col in directory:
             df[f"{col}_sin"] = np.sin(2 * np.pi * df[col] / directory[col])
             df[f"{col}_cos"] = np.cos(2 * np.pi * df[col] / directory[col])
         return df
 
-    def drain_rate(df):
-        df['drain_rate'] = df['']
+    def drain_rate():
+        df['drain_rate'] = (df['amount']/df['sender_balance_before']) * 100
+        return df
+
+    def account_emptied():
+        df['account_emptied'] = df['amount'] >= df['sender_balance_before']
+        df['account_emptied'] = df['account_emptied'].map({True: 1, False: 0})
+        return df
+    
+    df = day_mapper()
+    df = cyclic_encoder()
+    df = drain_rate()
+    df = account_emptied()
+    
+    return df
---
+++ utils/preprocecessing.py
@@ -0,0 +1,4 @@
+# This utility will perform the following
+#1. Delete columns that are not needed
+#2. Map the device column to binary(smartphone: 0, feature: 1)
+
