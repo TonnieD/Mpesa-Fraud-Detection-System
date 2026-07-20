d4385f295c75d08ee3701ca6a528acb065b7bf19
Anthony <ngangaanthony31@gmail.com>
2026-07-20 15:36:02 +0300

    API development for prediction

---
+++ inference/main.py
@@ -0,0 +1,95 @@
+#Library importations
+from fastapi import FastAPI, HTTPException
+import uvicorn
+from pydantic import BaseModel
+import pandas as pd
+import numpy as np
+import joblib
+from utils.preprocessing import preprocessing
+from utils.feature_engineering import feature_engineering
+
+
+
+#App initialization
+app = FastAPI()
+
+#Model & Encoder loading
+model = joblib.load('../models/best_model.pkl')
+
+#Pydantic Request Model
+class TransactionRequest(BaseModel):
+    transaction_id : str
+    amount : float
+    sender_balance_before : float
+    transaction_type : str
+    device_type : str
+    region : str
+    hour : int
+    day_of_week : str
+    month_2026 : int
+
+# Pydantic Response Model
+class TransactionResponse(BaseModel):
+    fraud_probability : float = None
+    risk_tier : str
+    recommended_action : str
+    action_rationale : str
+
+#Health status endpoint
+@app.get('/health_status')
+async def health_status():
+    return {'status': 'OK'}
+
+#Prediction Endpoint
+@app.post('/predict')
+async def predict(request:TransactionRequest):
+    try:
+        # Convert request to dataframe
+        data = request.dict()
+        df = pd.DataFrame([data])
+
+        # Perform feature engineering
+        df = feature_engineering(df)
+
+        #Check_hard coded rules
+        if df['account_emptied'].values[0] == 1:
+            return TransactionResponse(risk_tier = 'HIGH',
+            recommended_action = 'BLOCK',
+            action_rationale = 'Account Will Be Emptied')
+        elif df['drain_rate'].values[0] >= 100:
+            return TransactionResponse(risk_tier = 'HIGH',
+            recommended_action = 'BLOCK',
+            action_rationale = 'Transaction Exceeds Available Balance')
+
+        # Preprocess the data
+        df = preprocessing(df)
+
+        # Predict the fraud probability
+        fraud_probability = model.predict_proba(df)[0][1]
+
+        # Determine the risk tier
+        if fraud_probability >= 0.502:
+            risk_tier = 'HIGH'
+            recommended_action = 'BLOCK'
+            action_rationale = 'High fraud probability detected'
+        elif fraud_probability > 0.497:
+            risk_tier = 'MEDIUM'
+            recommended_action = 'CHALLENGE'
+            action_rationale = 'Medium fraud probability detected'
+        else:
+            risk_tier = 'LOW'
+            recommended_action = 'APPROVE'
+            action_rationale = 'Low fraud probability detected'
+
+        # Return the response
+        return TransactionResponse(
+            fraud_probability=fraud_probability,
+            risk_tier=risk_tier,
+            recommended_action=recommended_action,
+            action_rationale=action_rationale
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
--- inference/utils/feature_engineering.py
+++ inference/utils/feature_engineering.py
@@ -2,9 +2,9 @@
 # 1. Create new feature called drain_rate
 # 2. perform cyclic encoding from temporal features
 # 3. create new feature called account_emptied
+import numpy as np
 
 def feature_engineering(df):
-
     def day_mapper():
         mapper = {'Monday': 1, 'Tuesday': 2, 'Wednesday': 3, 'Thursday': 4, 'Friday': 5, 'Saturday': 6, 'Sunday': 7}
         df['day'] =  df['day_of_week'].map(mapper)
